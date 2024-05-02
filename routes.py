5eee47cdb6bce6f485048ba75ce1a37a35661087
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-02 14:15:52 +1200

    Added CSS Added side bar template Added sheet template

--- routes.py
+++ routes.py
@@ -2,21 +2,22 @@ from flask import Flask, render_template
 import sqlite3
 
 app=Flask(__name__)
+db='main.db'
 
 @app.route('/')
 def home():
     return "home"
 
 @app.route('/character/<id>')
-def characterMain(id):
-    conn=sqlite3.connect('main.db')
+def character_main(id):
+    conn=sqlite3.connect(db)
     cur=conn.cursor()
     cur.execute(f'SELECT * FROM Character WHERE Character_Id = {id}')
-    characterData=cur.fetchone()
-    if characterData==None:
+    character_data=cur.fetchone()
+    if character_data==None:
         return "INVALID CHARACTER"
-    print(characterData[1])
-    return render_template('CharacterMain.html',CharacterData=characterData)
+    print(character_data[1])
+    return render_template('CharacterMain.html',character=[character_data[1],character_data[0],character_data[10]] )
 
 
 if __name__ == "__main__":
