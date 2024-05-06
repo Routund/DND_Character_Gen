9b965c8cc6d1139101f90822650eca5e779c3f00
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-06 12:41:08 +1200

    Worked on adding skill bonus information to page Added level info table Added 2d list of skills

--- routes.py
+++ routes.py
@@ -4,11 +4,18 @@ import sqlite3
 app=Flask(__name__)
 db='main.db'
 
+skills = [
+    ["Athletics"],
+    ["Acrobatics","Sleight of hand","Stealth"],
+    ["Arcana","History","Investigation","Nature","Religion"],
+    ["Animal Handling","Insight","Medicine","Perception","Survival"],
+    ["Deception","Intimidation","Performance","Persuasion"]]
+
 @app.route('/')
 def home():
     return "home"
 
-# STATS GO STR, DEX, WIS, INT, CHA, CON
+# STATS GO STR, DEX, INT, WIS, CHA, CON
 
 @app.route('/character/<id>')
 def character_main(id):
@@ -18,11 +25,19 @@ def character_main(id):
     character_data=cur.fetchone()
     if character_data==None:
         return "INVALID CHARACTER"
-    print(character_data[1])
     cur.execute(f'SELECT Name FROM Race WHERE Race_Id = {character_data[2]}')
     race=cur.fetchone()
     cur.execute(f'SELECT Name FROM Class WHERE Class_Id = {character_data[3]}')
     classC=cur.fetchone()
+    proficiencies=character_data[9].split(',')
+    stats=character_data[8].split(',')
+
+    i=0
+    skillBonus=[]
+    while (i<5):
+        for skill in skills:
+            print("TO DO")
+
     return render_template('CharacterMain.html',character=[character_data[1],race[0],classC[0]])
 
 
